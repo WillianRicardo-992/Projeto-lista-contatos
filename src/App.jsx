e1cde9e757b4ac437954d3249ed1792f67048d09
Willian Ricardo <willianricardoleal@gmail.com>
2022-10-23 22:59:32 -0300

    Remoção do contato por objeto e id unico

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Contato from './componentes/Contato'
 import { v4 as chave } from 'uuid'
 
 export default function App() {
 
     //state
-    const [contato, setContato] = useState({ nome: '', telefone: '' })
+    const [contato, setContato] = useState({ id: '', nome: '', telefone: '' })
     const [listaContatos, setListaContatos] = useState([])
-    
+
     //useRef
     const inputNome = useRef()
     const inputTelefone = useRef()
@@ -16,7 +16,7 @@ export default function App() {
     function definirNome(event) {
         setContato({ ...contato, nome: event.target.value })
     }
- 
+
     function definirTelefone(event) {
         setContato({ ...contato, telefone: event.target.value })
     }
@@ -27,7 +27,7 @@ export default function App() {
         if (contato.nome === "" || contato.telefone === "") return
 
         //adicionar novo contato à lista
-        setListaContatos([...listaContatos, contato])
+        setListaContatos([...listaContatos, {...contato, id: chave()}])
 
         //limpar o contato
         setContato({ nome: '', telefone: '' })
@@ -37,15 +37,45 @@ export default function App() {
 
         //verificar se o contato ja existe
         let duplicado = listaContatos.find((ct) => ct.nome === contato.nome && ct.telefone === contato.telefone)
-        if(typeof duplicado === 'undefined') {
+        if (typeof duplicado === 'undefined') {
             inputTelefone.current.focus()
             return
         }
     }
+    function enterAdicionarCotato(event) {
+        if (event.code === "Enter") {
+            adicionarContato()
+        }
+    }
+
+    //persistencia do state
+    //carregar a listaContatos do localStorage
+    useEffect(()=> {
+        if(localStorage.getItem('meus_contatos') !== null){
+            setListaContatos(JSON.parse(localStorage.getItem('meus_contatos')))
+        }
+    }, [])
+
+    //atualizar a lista de contatos no localStorage
+    useEffect(() => {
+        localStorage.setItem('meus_contatos', JSON.stringify(listaContatos))
+    }, [listaContatos])
+
+    //limpar toda a lista
+    function limparStorage(){
+        setListaContatos([])
+    }
+
+    //remover um contato da ista
+    function removerContato(id){
+        let tmp = listaContatos.filter(ct => ct.id !== id)
+        setListaContatos(tmp)
+    }
+    
 
     return (
         <>
-            <h1>Minha lista de contatos</h1>
+            <h1 className='text-center'>Minha lista de contatos</h1>
             <hr />
             <div>
                 <label>Nome:</label><br />
@@ -53,13 +83,14 @@ export default function App() {
             </div>
             <div>
                 <label>Telefone</label><br />
-                <input type="text" ref={inputTelefone} onChange={definirTelefone} value={contato.telefone} />
+                <input type="text" ref={inputTelefone} onChange={definirTelefone} onKeyUp={enterAdicionarCotato} value={contato.telefone} />
             </div>
             <button onClick={adicionarContato}>Adicionar Contato</button>
+            <button onClick={limparStorage}>Limpar</button>
             <hr />
-            {/* apresentação da lista de contatos*/} 
+            {/* apresentação da lista de contatos*/}
             {listaContatos.map(ct => {
-                return <Contato key={chave()} nome={ct.nome} telefone={ct.telefone} />
+                return <Contato key={ct.id} id={ct.id} nome={ct.nome} telefone={ct.telefone} remover={removerContato} />
             })}
 
         </>
--- src/componentes/Contato.jsx
+++ src/componentes/Contato.jsx
@@ -4,6 +4,7 @@ export default function Contato(props){
     return(
     <div>
         {props.nome} - {props.telefone}
+        <button onClick={() => {props.remover(props.id)}}>Remover</button>
     </div>
     )
 }
