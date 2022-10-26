7507ada93c0483b0d57b6fd6f5fac646d204066d
Willian Ricardo <willianricardoleal@gmail.com>
2022-10-26 08:03:46 -0300

    finalização do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAdd, faList, faTrash } from '@fortawesome/free-solid-svg-icons'
 import Contato from './componentes/Contato'
 import { v4 as chave } from 'uuid'
+import './App.css'
 
 export default function App() {
 
@@ -27,7 +30,7 @@ export default function App() {
         if (contato.nome === "" || contato.telefone === "") return
 
         //adicionar novo contato à lista
-        setListaContatos([...listaContatos, {...contato, id: chave()}])
+        setListaContatos([...listaContatos, { ...contato, id: chave() }])
 
         //limpar o contato
         setContato({ nome: '', telefone: '' })
@@ -50,8 +53,8 @@ export default function App() {
 
     //persistencia do state
     //carregar a listaContatos do localStorage
-    useEffect(()=> {
-        if(localStorage.getItem('meus_contatos') !== null){
+    useEffect(() => {
+        if (localStorage.getItem('meus_contatos') !== null) {
             setListaContatos(JSON.parse(localStorage.getItem('meus_contatos')))
         }
     }, [])
@@ -62,36 +65,69 @@ export default function App() {
     }, [listaContatos])
 
     //limpar toda a lista
-    function limparStorage(){
+    function limparStorage() {
         setListaContatos([])
     }
 
     //remover um contato da ista
-    function removerContato(id){
+    function removerContato(id) {
         let tmp = listaContatos.filter(ct => ct.id !== id)
         setListaContatos(tmp)
     }
-    
+
 
     return (
         <>
-            <h1 className='text-center'>Minha lista de contatos</h1>
-            <hr />
-            <div>
-                <label>Nome:</label><br />
-                <input type="text" ref={inputNome} onChange={definirNome} value={contato.nome} />
+
+            <div className="container-fluid titulo">
+                <div className="row">
+                    <div className="col text-center">
+                        <h4 className='text-center'><FontAwesomeIcon icon={faList} className="me-3" />LISTA DE CONTATOS </h4>
+                    </div>
+                </div>
             </div>
-            <div>
-                <label>Telefone</label><br />
-                <input type="text" ref={inputTelefone} onChange={definirTelefone} onKeyUp={enterAdicionarCotato} value={contato.telefone} />
+
+            <div className="container-fluid formulario">
+                <div className="row">
+                    <div className="col p-3">
+
+                        <div className="row justify-content-center">
+                            <div className="col-10 col-sm-8 col-md-6 col-lg-4">
+                                <div className='mb-3'>
+                                    <label className='form-label'>Nome</label><br />
+                                    <input type="text" ref={inputNome} onChange={definirNome} value={contato.nome} className="form-control" />
+
+                                </div>
+
+                                <div>
+                                    <label className='form-label'>Telefone</label><br />
+                                    <input type="text" ref={inputTelefone} onChange={definirTelefone} onKeyUp={enterAdicionarCotato} value={contato.telefone} className="form-control" />
+                                </div>
+
+                                <div className="row mt-3">
+                                    <div className="col text-start">
+                                        <button onClick={limparStorage} className="btn btn-outline-warning">
+                                            <FontAwesomeIcon icon={faTrash} className="me-2" />Limpar</button>
+                                    </div>
+                                    <div className="col text-end">
+                                        <button onClick={adicionarContato} className="btn btn-outline-info"><FontAwesomeIcon icon={faAdd} className="me-2"  />Adicionar Contato</button>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                {/* apresentação da lista de contatos*/}
+                {listaContatos.map(ct => {
+                    return <Contato key={ct.id} id={ct.id} nome={ct.nome} telefone={ct.telefone} remover={removerContato} />
+                })}
+
             </div>
-            <button onClick={adicionarContato}>Adicionar Contato</button>
-            <button onClick={limparStorage}>Limpar</button>
-            <hr />
-            {/* apresentação da lista de contatos*/}
-            {listaContatos.map(ct => {
-                return <Contato key={ct.id} id={ct.id} nome={ct.nome} telefone={ct.telefone} remover={removerContato} />
-            })}
 
         </>
     )
--- src/componentes/Contato.jsx
+++ src/componentes/Contato.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import './componente.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhoneVolume, faTrashAlt, faUser, faUserTie } from '@fortawesome/free-solid-svg-icons'
 
-export default function Contato(props){
-    return(
-    <div>
-        {props.nome} - {props.telefone}
-        <button onClick={() => {props.remover(props.id)}}>Remover</button>
-    </div>
+export default function Contato(props) {
+    return (
+        <div className="mx-2">
+            <div className="container componente-contato my-4">
+                <div className="row">
+                    <div className="col p-2">
+                        <h4>
+                            <FontAwesomeIcon icon={faUser} className="me-3" />
+                            {props.nome}
+                        </h4>
+                    </div>
+                    <div className="col P-2">
+                        <h4>
+                            <FontAwesomeIcon icon={faPhoneVolume} className="me-3" />
+                            {props.telefone}
+                        </h4>
+                    </div>
+                    <div className="col p-2 text-end">
+                        <h4>
+                            <FontAwesomeIcon
+                                icon={faTrashAlt} className="me-3"
+                                onClick={() => { props.remover(props.id) }} />
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
